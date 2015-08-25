4912aef1ec7e7550eba599f6761dae042674993b
jhostetter <josiahhostetter@gmail.com>
2015-08-25 12:04:43 -0700

    js functionality
    create and test javascript functionality on child theme

--- themifyflow-child/functions.php
+++ themifyflow-child/functions.php
@@ -14,3 +14,10 @@ endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css' );
 
 // END ENQUEUE PARENT ACTION
+
+
+//Custom js script function
+function flow_child_scripts() {
+    wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/js/intern-custom.js' );
+}
+add_action( 'wp_enqueue_scripts', 'flow_child_scripts' );
