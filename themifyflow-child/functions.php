cb795853f9e0874a6b4fad408a572e522776f595
jhostetter <josiahhostetter@gmail.com>
2015-08-26 15:53:02 -0700

    Contact Form - JS & CSS completed
    Completed functionality for Contact form selection:
    - javascript
    - css

--- themifyflow-child/functions.php
+++ themifyflow-child/functions.php
@@ -1,27 +1,14 @@
 <?php
-// Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
 
-// BEGIN ENQUEUE PARENT ACTION
-// AUTO GENERATED - Do not modify or remove comment markers above or below:
-
-        
-if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
-    function chld_thm_cfg_parent_css() {
-        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css' );
-    }
-endif;
-add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css' );
-
-// END ENQUEUE PARENT ACTION
-
-
+//Enqueues child theme stylesheet, loading first the parent theme stylesheet.
+function themify_child_styles() {
+wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'themify_child_styles' );
 
 
 //Custom js script function
-
-add_action('wp_enqueue_scripts', 'themify_add_scripts');
-
-function themify_add_scripts(){
-    wp_enqueue_script( 'script-name', get_stylesheet_directory_uri() . '/js/intern-custom.js' )
+function flow_child_scripts() {
+    wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/js/intern-custom.js' );
 }
+add_action( 'wp_enqueue_scripts', 'flow_child_scripts' );
