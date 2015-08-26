430b8f2cba03f064d3266bd08fba7363b971c252
jhostetter <josiahhostetter@gmail.com>
2015-08-25 18:07:35 -0700

    js test completed
    js working properly. ready for new js.

--- themifyflow-child/functions.php
+++ themifyflow-child/functions.php
@@ -16,8 +16,12 @@ add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css' );
 // END ENQUEUE PARENT ACTION
 
 
+
+
 //Custom js script function
-function flow_child_scripts() {
-    wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/js/intern-custom.js' );
+
+add_action('wp_enqueue_scripts', 'themify_add_scripts');
+
+function themify_add_scripts(){
+    wp_enqueue_script( 'script-name', get_stylesheet_directory_uri() . '/js/intern-custom.js' )
 }
-add_action( 'wp_enqueue_scripts', 'flow_child_scripts' );
